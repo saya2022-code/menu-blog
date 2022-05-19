736ab9f61107962351d3fc68cf56de45c2248584
saya2022-code <s.nishizaka@g.dreamcareer.co.jp>
2022-05-19 15:17:54 +0900

    PHP 7.

--- index.php
+++ index.php
@@ -10,25 +10,27 @@ class Menu{
       //⑤「コンストラクタ」というメソッドを定義
         //→ インスタンスを生成すると、自動でコンストラクタが呼ばれる＝処理やプロパティを追加できる
         //public function __construct(引数) { 処理内容 }
-          public function __construct(){  // __constructはアンダーバーが2つ
-            echo '１つのメニューが作られました';
-            echo '<br>'; //改行
-          }
-
-          //⑤-3　コンストラクタの引数に個別の情報が入る。
-          public function __construct($name){ //この$nameはプロパティ名とは別のもの(自由に)
-            echo $name;
+          // public function __construct(){  // __constructはアンダーバーが2つ
+          //   echo '１つのメニューが作られました';
+          //   echo '<br>'; //改行
+          // }
+          
+          //⑤-3　コンストラクタの引数に個別の情報(⑤-2)が入る。
+          public function __construct($name) {
+            // nameプロパティに引数の$nameを代入してください
+            $this->name = $name; //これで④の$curry->name = 'CURRY';を消しても表示される
+            
           }
 
    //③-2 public function メソッド名() { 処理内容 }
     public function hello(){
       echo '私はMenuクラスのインスタンスです';
-      echo '<br>'; //改行
+      echo '<br>'; 
 
       //④-2 $thisを使って値を出力する
        //$this=メソッド内でインスタンスのプロパティやメソッドにアクセスする変数
       echo '私は'.$this->name.'です';
-      echo '<br>'; //改行
+      echo '<br>'; 
     }
 }
 
@@ -40,15 +42,16 @@ class Menu{
 
     //⑤-2 コンストラクタに渡す引数(個別の情報)を入れる
     $curry = new Menu('CURRY');
+    $pasta = new Menu('PASTA');
 
 //④ ③で定義した共通プロパティなどに「個別の情報」をセットする
  //インスタンス->プロパティ名 = '個別の情報'
   //この時のプロパティ名は＄がいらない。インスタンス名とかぶるから。
-  $curry->name = 'CURRY';
-  $pasta->name = 'PASTA';
+  // $curry->name = 'CURRY'; //⑤-3のコンストラクタや$thisが設定されていれば、これは消して良い。
+  // $pasta->name = 'PASTA';
 
   //echoで④が出力できるか確認する→成功
-    echo $curry->name;
+    echo $curry->name; //⑤-3が出力
     echo '<br>'; //改行
     echo $pasta->name;
     echo '<br>'; //改行
