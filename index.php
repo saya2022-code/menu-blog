9616a337324030baf570350bcbafb16970fd3c0a
saya2022-code <s.nishizaka@g.dreamcareer.co.jp>
2022-05-19 15:52:25 +0900

    PHP 9.

--- index.php
+++ index.php
@@ -23,15 +23,15 @@ class Menu{
           }
 
    //③-2 public function メソッド名() { 処理内容 }
-    public function hello(){
-      echo '私はMenuクラスのインスタンスです';
-      echo '<br>'; 
+    // public function hello(){
+    //   echo '私はMenuクラスのインスタンスです';
+    //   echo '<br>'; 
 
       //④-2 $thisを使って値を出力する
        //$this=メソッド内でインスタンスのプロパティやメソッドにアクセスする変数
-      echo '私は'.$this->name.'です';
-      echo '<br>'; 
-    }
+    //   echo '私は'.$this->name.'です';
+    //   echo '<br>'; 
+    // }
 }
 
 //②クラスのインスタンス(カレーやパスタなどの単品)を生成
@@ -41,8 +41,16 @@ class Menu{
   // $pasta = new Menu();
 
     //⑤-2 コンストラクタに渡す引数(個別の情報)を入れる
+    $juice = new Menu('JUICE');
+    $coffee = new Menu('COFFEE');
     $curry = new Menu('CURRY');
     $pasta = new Menu('PASTA');
+
+    //⑦ ⑤-2のインスタンスを配列にまとめる
+    //$配列名 = array( $変数名1,$変数名2..);
+    $menus = array(
+      $juice,$coffee,$curry,$pasta
+    );
 
 //④ ③で定義した共通プロパティなどに「個別の情報」をセットする
  //インスタンス->プロパティ名 = '個別の情報'
@@ -51,10 +59,43 @@ class Menu{
   // $pasta->name = 'PASTA';
 
   //echoで④が出力できるか確認する→成功
-    echo $curry->name; //⑤-3が出力
-    echo '<br>'; //改行
-    echo $pasta->name;
-    echo '<br>'; //改行
-    echo $curry->hello(); //hello()を呼び出しているインスタンス($curry)が$thisに入る(④-2)
-    echo $pasta->hello(); //hello()を呼び出しているインスタンス($pasta)が$thisに入る(④-2)
+    // echo $curry->name; //⑤-3が出力 → ⑥のHTMLより、これもあとでいらなくなる。
+    // echo '<br>'; //改行
+    // echo $pasta->name;
+    // echo '<br>'; //改行
+    // echo $curry->hello(); //hello()を呼び出しているインスタンス($curry)が$thisに入る(④-2)
+    // echo $pasta->hello(); //hello()を呼び出しているインスタンス($pasta)が$thisに入る(④-2)
 ?>
+
+<!-- ここからHTML(練習) -->
+
+<!-- ⑥ php内で設定したプロパティ名などを使い、個別の情報を出力 -->
+  <!-- 1行で済む場合、「;」を省略できる -->
+<!-- <p><?php echo $curry->name ?></p> 
+<p><?php echo $pasta->name ?></p>  -->
+
+
+<!-- ここからHTML(本番) -->
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Café Progate</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+  <link href='https://fonts.googleapis.com/css?family=Pacifico|Lato' rel='stylesheet' type='text/css'>
+</head>
+<body>
+  <div class="menu-wrapper container">
+    <h1 class="logo">Café Progate</h1>
+    <div class="menu-items">
+
+    <!-- ⑧foreach文(endforeach)で配列$menuの要素を出力 -->
+    <!-- 配列$menusの要素を変数$menuとするforeach文 -->
+    <?php foreach($menus as $menu):?>
+      <h3><?php echo $menu->name?></h3>
+      <?php endforeach?>
+
+  </div>
+  </div>
+</body>
+</html>
